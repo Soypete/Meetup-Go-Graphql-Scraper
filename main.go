7ad9e0eb7465bbe82beabca65b0bfea8d1711741
soypete <petersonmiriah@gmail.com>
2023-04-10 15:44:15 -0600

    meetup: write the meetup data into a csv file
    Signed-off-by: soypete <petersonmiriah@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/soypete/Metup-Go-Graphql-Scraper/auth"
@@ -17,83 +16,8 @@ func main() {
 
 	// TODO(soypete): create csv with relevant data.
 	// for list of data in README.md
-	err = buildDataSet(meetupClient)
+	err = meetupClient.BuildDataSet()
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-type analytics struct {
-	Groups map[string]groupAnalytics // key in the group's meetup ID
-}
-type groupAnalytics struct {
-	ID     string
-	Name   string
-	Events map[string]eventAnalytics // key is the group's meetup ID
-}
-
-type eventAnalytics struct {
-	ID      string
-	Title   string
-	Date    string
-	RSVPNum int
-}
-
-func getGroups(meetupClient meetup.Client) map[string]groupAnalytics {
-	var cursor string
-	hasNextPage := true
-	groupMap := make(map[string]groupAnalytics)
-	for hasNextPage {
-		groups, err := meetupClient.GetListOfGroups(cursor)
-		if err != nil {
-			log.Fatal(err)
-		}
-		for _, g := range groups.Data.ProNetwork.GroupsSearch.Edges {
-			if _, ok := groupMap[g.Node.ID]; ok {
-				continue
-			}
-			groupMap[g.Node.ID] = groupAnalytics{
-				ID:   g.Node.ID,
-				Name: g.Node.Name,
-			}
-		}
-		cursor = groups.Data.ProNetwork.GroupsSearch.PageInfo.EndCursor
-		hasNextPage = groups.Data.ProNetwork.GroupsSearch.PageInfo.HasNextPage
-	}
-	return groupMap
-}
-func buildDataSet(meetupClient meetup.Client) error {
-	var a analytics
-	groupMap := getGroups(meetupClient)
-	var cursor string
-	hasNextPage := true
-	eventMap := make(map[string]eventAnalytics)
-	for hasNextPage {
-		events := meetupClient.GetListOfEvents(cursor)
-		for _, e := range events.Data.ProNetwork.EventsSearch.Edges {
-			var ok bool
-			if _, ok = eventMap[e.Node.ID]; !ok {
-				eventMap[e.Node.ID] = eventAnalytics{
-					ID:    e.Node.ID,
-					Title: e.Node.Title,
-					Date:  e.Node.DateTime,
-				}
-			}
-			var g groupAnalytics
-			if g, ok = groupMap[e.Node.Group.ID]; !ok || g.Events == nil {
-				g.Events = make(map[string]eventAnalytics)
-			}
-			g.Events[e.Node.ID] = eventAnalytics{
-				ID:    e.Node.ID,
-				Title: e.Node.Title,
-				Date:  e.Node.DateTime,
-			}
-			groupMap[e.Node.Group.ID] = g
-		}
-		cursor = events.Data.ProNetwork.EventsSearch.PageInfo.EndCursor
-		hasNextPage = events.Data.ProNetwork.EventsSearch.PageInfo.HasNextPage
-	}
-	a.Groups = groupMap
-	fmt.Println(a.Groups)
-	return nil
 }
